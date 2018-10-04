abc7f1de5a4b28c752d7402078bc7f638757983d
Tim2.0 <B10023055@yuntech.edu.tw>
2018-10-04 17:14:42 +0800

    Testing a Component

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,17 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld/>
+    <MessageList :messages="messages"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
+import MessageList from './components/MessageList'
 
 export default {
-  name: 'App',
+  name: 'app',
+  data: () => ({ messages: ['Hey John', 'Howdy Paco'] }),
   components: {
-    HelloWorld
+    MessageList
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
